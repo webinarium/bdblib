059f2b0930de032b66bb5f7d3c67c605349633ce
Artem.Rodygin <Artem.Rodygin@d1772d03-883a-4eaa-86cd-6591a91b474d>
2010-10-24 14:38:48 +0000

    Version 1.0.2 is tagged.

---
+++ tags/bdblib-1.0.2/src/bdb/src/index.cc
@@ -0,0 +1,197 @@
+//--------------------------------------------------------------------------------------------------
+//
+//  BDB Library
+//  Copyright (C) 2009  Artem Rodygin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------------------------------
+//  Author                  Date            Description of modifications
+//--------------------------------------------------------------------------------------------------
+//  Artem Rodygin           2009-11-18      Initial creation.
+//--------------------------------------------------------------------------------------------------
+
+/** @file bdb/src/index.cc
+ * Contains implementation of class "bdb::index".
+ * @author Artem Rodygin
+ */
+
+#include <bdb.h>
+
+// Standard C/C++ Libraries
+#include <exception>
+#include <string>
+
+// Protocol Buffers
+#include <google/protobuf/message.h>
+
+// Berkeley DB
+#include <db.h>
+
+// Check for Berkeley DB version
+#if (DB_VERSION_MAJOR < 4) || (DB_VERSION_MAJOR == 4) && (DB_VERSION_MINOR < 7)
+#error Berkeley DB 4.7 or later is required.
+#endif
+
+// C++ Logging Library
+#include <log4cplus/logger.h>
+
+//--------------------------------------------------------------------------------------------------
+//  Implementation of class "bdb::index".
+//--------------------------------------------------------------------------------------------------
+
+namespace bdb
+{
+
+using std::string;
+
+using google::protobuf::Message;
+
+//--------------------------------------------------------------------------------------------------
+//  Constructors/destructors.
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * Opens the index, automatically creating it if required.
+ *
+ * @throw bdb::exception BDB_ERROR_UNKNOWN - unknown error.
+ */
+index::index (table            * tbl,       /**< [in] Master table.                                                  */
+              const char       * name,      /**< [in] Name of the index.                                             */
+              index_callback     fn_idx,    /**< [in] Indexing function (see "Db::associate()").                     */
+              compare_callback   fn_cmp,    /**< [in] Index comparision function (see "Db::set_bt_compare()").       */
+              compare_callback   fn_dup,    /**< [in] Duplicates comparision function (see "Db::set_dup_compare()"). */
+              bool               unique)    /**< [in] Whether the index should contain unique keys only.             */
+  : table(name)
+{
+    LOG4CPLUS_TRACE(logger, "[bdb::index::index] ENTER");
+    LOG4CPLUS_DEBUG(logger, "[bdb::index::index] name = " << name);
+    LOG4CPLUS_DEBUG(logger, "[bdb::index::index] unique = " << unique);
+
+    m_database = tbl->m_database;
+
+    int res = db_create(&m_db, tbl->m_db->get_env(tbl->m_db), 0);
+
+    if (fn_cmp != NULL)
+    {
+        if (res == 0) res = m_db->set_bt_compare(m_db, fn_cmp);
+    }
+
+    if (!unique)
+    {
+        if (fn_dup != NULL)
+        {
+            if (res == 0) res = m_db->set_dup_compare(m_db, fn_dup);
+        }
+
+        if (res == 0) res = m_db->set_flags(m_db, DB_DUPSORT);
+    }
+
+    if (res == 0)
+    {
+        res = m_db->open(m_db,
+                         m_database->m_txns.top(),
+                         get_filename().c_str(),
+                         m_name.c_str(),
+                         DB_BTREE,
+                         DB_THREAD | DB_CREATE,
+                         0);
+    }
+
+    if (res == 0) res = tbl->m_db->associate(tbl->m_db, m_database->m_txns.top(), m_db, fn_idx, DB_CREATE);
+
+    if (res != 0)
+    {
+        LOG4CPLUS_WARN(logger, "[bdb::index::index] " << db_strerror(res));
+        if (m_db != NULL) m_db->close(m_db, 0);
+        throw exception(BDB_ERROR_UNKNOWN);
+    }
+
+    LOG4CPLUS_TRACE(logger, "[bdb::index::index] EXIT");
+}
+
+//--------------------------------------------------------------------------------------------------
+//  Public interface.
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * Adds a foreign key to specified table.
+ * The foreign key means that any key, inserted into this index, must exist in the foreign table.
+ *
+ * @throw bdb::exception BDB_ERROR_UNKNOWN - unknown error.
+ */
+void index::add_foreign (table * foreign,   /**< [in] Master table. */
+                         bool    cascade)   /**< [in] Whether a cascade removal should be enabled: \n
+                                                      true  - key from this index will be deleted automatically
+                                                              when related key in master table is being deleted, \n
+                                                      false - it's unable to delete related key in master table
+                                                              until same key is present in the index. */
+{
+    LOG4CPLUS_TRACE(logger, "[bdb::index::add_foreign] ENTER (cascade)");
+    LOG4CPLUS_DEBUG(logger, "[bdb::index::add_foreign] cascade = " << cascade);
+
+    int res = foreign->m_db->associate_foreign(foreign->m_db, m_db, NULL,
+                                               cascade ? DB_FOREIGN_CASCADE : DB_FOREIGN_ABORT);
+
+    if (res != 0)
+    {
+        LOG4CPLUS_WARN(logger, "[bdb::index::add_foreign] " << db_strerror(res));
+        throw exception(BDB_ERROR_UNKNOWN);
+    }
+
+    LOG4CPLUS_TRACE(logger, "[bdb::index::add_foreign] EXIT");
+}
+
+/**
+ * Adds a foreign key to specified table.
+ * The foreign key means that any key, inserted into this index, must exist in the foreign table.
+ *
+ * @throw bdb::exception BDB_ERROR_UNKNOWN - unknown error.
+ */
+void index::add_foreign (table            * foreign,    /**< [in] Master table. */
+                         nullify_callback   nullify)    /**< Nullification callback function, which is used
+                                                             during cascade removal to update affected data. */
+{
+    LOG4CPLUS_TRACE(logger, "[bdb::index::add_foreign] ENTER (nullify)");
+
+    int res = foreign->m_db->associate_foreign(foreign->m_db, m_db, nullify, DB_FOREIGN_NULLIFY);
+
+    if (res != 0)
+    {
+        LOG4CPLUS_WARN(logger, "[bdb::index::add_foreign] " << db_strerror(res));
+        throw exception(BDB_ERROR_UNKNOWN);
+    }
+
+    LOG4CPLUS_TRACE(logger, "[bdb::index::add_foreign] EXIT");
+}
+
+/**
+ * Checks whether a record with specified key exists in the index.
+ * <strong>NOTE:</strong> index can have duplicated keys.
+ *
+ * @return true  - at least one record exists, \n
+ *         false - no record is found.
+ */
+bool index::exists (const Message * key)    /**< [in] Key of the record to be checked. */
+{
+    LOG4CPLUS_TRACE(logger, "[bdb::index::exists] ENTER");
+    bool res = table::exists(key);
+    LOG4CPLUS_TRACE(logger, "[bdb::index::exists] EXIT = " << res);
+
+    return res;
+}
+
+}
+
+//--------------------------------------------------------------------------------------------------
---
+++ tags/bdblib-1.0.2/src/bdb/src/sequence.cc
@@ -0,0 +1,160 @@
+//--------------------------------------------------------------------------------------------------
+//
+//  BDB Library
+//  Copyright (C) 2009  Artem Rodygin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//--------------------------------------------------------------------------------------------------
+//  Author                  Date            Description of modifications
+//--------------------------------------------------------------------------------------------------
+//  Artem Rodygin           2009-12-24      Initial creation.
+//--------------------------------------------------------------------------------------------------
+
+/** @file bdb/src/sequence.cc
+ * Contains implementation of class "bdb::sequence".
+ * @author Artem Rodygin
+ */
+
+#include <bdb.h>
+
+// Standard C/C++ Libraries
+#include <cerrno>
+#include <iostream>
+#include <stdint.h>
+
+// Berkeley DB
+#include <db.h>
+
+// Check for Berkeley DB version
+#if (DB_VERSION_MAJOR < 4) || (DB_VERSION_MAJOR == 4) && (DB_VERSION_MINOR < 7)
+#error Berkeley DB 4.7 or later is required.
+#endif
+
+// C++ Logging Library
+#include <log4cplus/logger.h>
+
+//--------------------------------------------------------------------------------------------------
+//  Implementation of class "bdb::sequence".
+//--------------------------------------------------------------------------------------------------
+
+namespace bdb
+{
+
+//--------------------------------------------------------------------------------------------------
+//  Constructors/destructors.
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * Opens the sequence.
+ * If "create" is "true" and sequence doesn't exist, then creates it.
+ *
+ * @throw bdb::exception BDB_ERROR_NOT_FOUND - the sequence is not found.
+ * @throw bdb::exception BDB_ERROR_EXISTS    - the sequence already exists.
+ * @throw bdb::exception BDB_ERROR_UNKNOWN   - unknown error.
+ */
+sequence::sequence (database   * db,        /**< [in] Database of the sequence.                           */
+                    const char * name,      /**< [in] Name of the sequence.                               */
+                    bool         create)    /**< [in] Whether to create the sequence if it doesn't exist. */
+  : m_database(db),
+    m_seq(NULL)
+{
+    LOG4CPLUS_TRACE(logger, "[bdb::sequence::sequence] ENTER");
+    LOG4CPLUS_DEBUG(logger, "[bdb::sequence::sequence] name = " << name);
+    LOG4CPLUS_DEBUG(logger, "[bdb::sequence::sequence] create = " << create);
+
+    int res = db_sequence_create(&m_seq, m_database->m_seq, 0);
+
+    if (res == 0) res = m_seq->set_flags(m_seq, DB_SEQ_INC);
+    if (res == 0) res = m_seq->set_cachesize(m_seq, 0);
+    if (res == 0) res = m_seq->initial_value(m_seq, 1);
+
+    u_int32_t flags = DB_THREAD;
+
+    if (create)
+    {
+        flags |= DB_CREATE | DB_EXCL;
+    }
+
+    DBT key;
+
+    memset(&key, 0, sizeof(DBT));
+    key.data = (void *) name;
+    key.size = (u_int32_t) strlen(name);
+
+    if (res == 0) res = m_seq->open(m_seq, m_database->m_txns.top(), &key, flags);
+
+    if (res != 0)
+    {
+        LOG4CPLUS_WARN(logger, "[bdb::sequence::sequence] " << db_strerror(res));
+
+        if (m_seq != NULL) m_seq->close(m_seq, 0);
+
+        switch (res)
+        {
+            case ENOENT:    throw exception(BDB_ERROR_NOT_FOUND);
+            case EEXIST:    throw exception(BDB_ERROR_EXISTS);
+            default:        throw exception(BDB_ERROR_UNKNOWN);
+        }
+    }
+
+    LOG4CPLUS_TRACE(logger, "[bdb::sequence::sequence] EXIT");
+}
+
+/**
+ * Closes the sequence.
+ */
+sequence::~sequence () throw ()
+{
+    LOG4CPLUS_TRACE(logger, "[bdb::sequence::~sequence] ENTER");
+
+    if (m_seq != NULL)
+    {
+        m_seq->close(m_seq, 0);
+    }
+
+    LOG4CPLUS_TRACE(logger, "[bdb::sequence::~sequence] EXIT");
+}
+
+//--------------------------------------------------------------------------------------------------
+//  Public interface.
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * Generates next unique identifier and returns it.
+ *
+ * @return 64-bit integer value.
+ * @throw bdb::exception BDB_ERROR_UNKNOWN - unknown error.
+ */
+int64_t sequence::id ()
+{
+    LOG4CPLUS_TRACE(logger, "[bdb::sequence::id] ENTER");
+
+    db_seq_t id;
+    int res = m_seq->get(m_seq, m_database->m_txns.top(), 1, &id, 0);
+
+    if (res != 0)
+    {
+        LOG4CPLUS_WARN(logger, "[bdb::sequence::id] " << db_strerror(res));
+        throw exception(BDB_ERROR_UNKNOWN);
+    }
+
+    LOG4CPLUS_TRACE(logger, "[bdb::sequence::id] EXIT = " << id);
+
+    return (int64_t)id;
+}
+
+}
+
+//--------------------------------------------------------------------------------------------------
