64d09bf4b1505a925d27ccd6f0a0746d9241c473
Artem.Rodygin <Artem.Rodygin@d1772d03-883a-4eaa-86cd-6591a91b474d>
2011-01-16 15:06:09 +0000

    Upgrade 'log4cplus' up to version 1.0.4.

--- src/log4cplus/src/global-init.cxx
+++ src/log4cplus/src/global-init.cxx
@@ -29,7 +29,12 @@ namespace log4cplus
 {
 
 
+//! Defined here, used by initializeLayout().
+helpers::Time TTCCLayout_time_base;
+
+
 void initializeFactoryRegistry();
+void initializeLayout ();
 
 
 void initializeLog4cplus()
@@ -43,6 +48,7 @@ void initializeLog4cplus()
     getNDC();
     Logger::getRoot();
     initializeFactoryRegistry();
+    initializeLayout ();
 
     initialized = true;
 }
